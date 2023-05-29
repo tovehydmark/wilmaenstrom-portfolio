3cc70e244efa215ec99d7143a7a9de90a6ff9663
tovehydmark <tovehydmark@gmail.com>
2023-05-29 22:25:57 +0200

    remove blog and add contact

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/app/components/Footer';
+import Header from '@/app/components/Header';
+
+const Contact = () => {
+  return (
+    <>
+      <Header></Header>
+
+      <h1>Wilmas kontaktsida</h1>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Contact;
