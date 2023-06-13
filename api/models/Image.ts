0e8a3428ffcf78f4f697da23f37ddbe423d1c684
tovehydmark <tovehydmark@gmail.com>
2023-06-13 19:45:45 +0200

    model for images

--- api/models/Image.ts
+++ api/models/Image.ts
@@ -3,8 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 const ImageSchema = new Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: String, required: true },
+    // description: { type: String, required: true },
+    // image: { type: String, required: true },
   },
   { timestamps: true },
 );
