8f00d0aba6d68034a46efae508511c1ad3f12512
tovehydmark <tovehydmark@gmail.com>
2023-06-19 10:00:03 +0200

    change model

--- src/app/api/models/Image.ts
+++ src/app/api/models/Image.ts
@@ -1,28 +1,15 @@
-// import { Model, model, models, Schema } from 'mongoose';
-// import { ModelName } from './modelnames';
-
-// const ImageSchema = new Schema({
-//   title: { type: String, required: true },
-//   description: { type: String, required: false },
-//   image: { type: String, required: true },
-// });
-
-// export type ImageDocument = typeof ImageSchema;
-
-// export default (models[ModelName.Image] || model(ModelName.Image, ImageSchema)) as Model<ImageDocument>;
-
-import { Model, model, models, Schema, Document } from 'mongoose';
+import { model, models, Schema, Document } from 'mongoose';
 import { ModelName } from './modelnames';
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: false },
+  src: { type: String, required: false },
   image: { type: String, required: true },
 });
 
 export type ImageDocument = Document & {
   title: string;
-  description: string;
+  src: string;
   image: string;
 };
 
