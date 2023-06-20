5a6653e53a531fd792b5b22e020a4b09878c7276
tovehydmark <tovehydmark@gmail.com>
2023-06-20 23:29:58 +0200

    styling

--- src/app/components/Menu/Menu.styled.ts
+++ src/app/components/Menu/Menu.styled.ts
@@ -4,35 +4,39 @@ export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  text-align: center;
   height: 100vh;
-  text-align: left;
+  width: 100vw;
   position: fixed;
   top: 0;
   left: 0;
-  background-color: #151515; //Todo: variables for color
-
+  background-color: #151515;
   transition: transform 0.3s ease-in-out;
   transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
 
-  //TODO: fixa variabler för bredd
   @media (min-width: 1061px) {
     display: none;
   }
 
-  a {
-    padding-left: 2rem;
-    font-size: 2rem;
-    text-transform: uppercase;
-    padding: 2rem 0;
-    font-weight: bold;
-    text-decoration: none;
-    transition: color 0.3s linear;
-    font-size: 1.5rem;
-    text-align: center;
+  ul {
+    list-style: none;
+    padding: 0;
 
-    &:hover {
-      //Todo: variabler
-      color: #ffd301;
+    li {
+      margin-bottom: 2rem;
+    }
+    a {
+      font-size: 2rem;
+      text-transform: uppercase;
+      font-weight: bold;
+      text-decoration: none;
+      transition: color 0.3s linear;
+      font-size: 1.5rem;
+      text-align: center;
+
+      &:hover {
+        color: #ffd301;
+      }
     }
   }
 `;
