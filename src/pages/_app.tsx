973605fb67f7d628fd695c10430bd126f152381a
tovehydmark <tovehydmark@gmail.com>
2023-06-26 16:33:07 +0200

    change font for h1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import Menu from '@/app/components/Menu';
 import RootLayout from '@/app/layout';
 import { useState } from 'react';
 
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Playfair_Display } from 'next/font/google';
 
 // If loading a variable font, you don't need to specify the font weight
-const montserrat = Montserrat({ subsets: ['latin'] });
+export const montserrat = Montserrat({ subsets: ['latin'], display: 'swap' });
+export const playfair_display = Playfair_Display({ subsets: ['latin'], display: 'swap' });
 
 function MyApp({ Component, pageProps }) {
   const [open, setOpen] = useState<boolean>(false);
@@ -16,6 +17,9 @@ function MyApp({ Component, pageProps }) {
       <style jsx global>{`
         html {
           font-family: ${montserrat.style.fontFamily};
+        }
+        h1 {
+          font-family: ${playfair_display.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
