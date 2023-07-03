fb4d99bb5f00fddce11b0e3d6d65174cedc1a2c9
tovehydmark <tovehydmark@gmail.com>
2023-07-03 12:42:01 +0200

    Styling about me page and navigation

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,10 +4,12 @@ const Contact = () => {
   return (
     <>
       <Layout>
-        <h1>Contact</h1>
-        <p>Are you interested in my work? Please contact me at</p>
-        <p>Email: wilmaenstrom@gmail.com</p>
-        <p>Phone: +4670 7770 7070</p>
+        <section className="contact-container">
+          <h1>Contact</h1>
+          <p>Are you interested in my work? Please contact me at</p>
+          <p>Email: wilmaenstrom@gmail.com</p>
+          <p>Phone: +4670 7770 7070</p>
+        </section>
       </Layout>
     </>
   );
