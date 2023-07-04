a310e1effa1de945dfe3cfb4d36d89aea8b89845
tovehydmark <tovehydmark@gmail.com>
2023-07-04 23:45:56 +0200

    dashboard created

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
+import { Router, useRouter } from 'next/router';
 
 const Admin = () => {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push('/admin/dashboard');
+  };
   return (
     <>
-      <form className="admin-login-section">
+      <form className="admin-login-section" onSubmit={handleLogin}>
         <label htmlFor="username">Användarnamn:</label>
         <input type="text" id="username" />
         <label htmlFor="password">Lösenord:</label>
         <input type="password" id="password" />
 
-        <button>Logga in</button>
+        <button type="submit">Logga in</button>
       </form>
       <Link href="/admin/createAccount">Skapa konto</Link>
     </>
