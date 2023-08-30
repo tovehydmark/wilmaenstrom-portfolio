4dd43c6bd0f3e479f2b8aa64ad6e67b5a80dec57
tovehydmark <tovehydmark@gmail.com>
2023-08-30 22:23:45 +0200

    change model for base64 image upload

--- src/app/api/models/Image.ts
+++ src/app/api/models/Image.ts
@@ -2,15 +2,11 @@ import { model, models, Schema, Document } from 'mongoose';
 import { ModelName } from './modelnames';
 
 const ImageSchema = new Schema({
-  title: { type: String, required: true },
-  src: { type: String, required: false },
   image: { type: String, required: true },
 });
 
 export type ImageDocument = Document & {
-  filename: string;
-  contentType: string;
-  data: string;
+  image: string;
 };
 
 export default models[ModelName.Image] || model<ImageDocument>(ModelName.Image, ImageSchema);
