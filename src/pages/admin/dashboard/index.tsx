37dd5a264ec93e6c4cdc90ad7fced5cc96ccb68b
tovehydmark <tovehydmark@gmail.com>
2023-08-30 22:47:38 +0200

    upload image base64 backend works

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -6,28 +6,9 @@ const Dashboard = () => {
   //Enable to delete image on X
   //Post new image from here
 
-  const postImageString = async () => {
-    const body = { title: '/BIBIB', src: '/mucha-4.jpeg' };
-
-    try {
-      let response = await fetch('/api/postImage', {
-        method: 'POST',
-        body: JSON.stringify(body),
-
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
-        },
-      });
-      const data: ImageDocument[] = await response.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <>
       <h1>Post images</h1>
-      <button onClick={postImageString}>Posta</button>
       <ImageUploadForm></ImageUploadForm>
     </>
   );
--- src/pages/api/postImage.ts
+++ src/pages/api/postImage.ts
@@ -1,93 +1,18 @@
-import { MongoClient } from 'mongodb';
-// import formidable from 'formidable';
-import { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '@/app/lib/mongodb';
-
-const formidable = require('formidable');
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export async function postImage(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const form = new formidable.IncomingForm();
+  try {
+    const client = await clientPromise;
+    const db = client.db('wilma-portfolio');
 
-    // console.log('form', form);
+    const result = await db.collection('images').insertOne({ image: req.body });
 
-    form.parse(req, async (err, fields, files) => {
-      if (err) {
-        console.error('Formidable error:', err);
-        res.status(500).send('Error parsing form data.');
-        return;
-      }
-
-      //Change any typing
-      const imageFile: any = files.image;
-
-      // console.log('imageFile', imageFile);
-
-      try {
-        const client = await clientPromise;
-        const db = client.db('wilma-portfolio');
-
-        console.log('imageFile', imageFile);
-
-        const imageData = {
-          filename: imageFile[0].originalFilename,
-          contentType: imageFile[0].mimetype,
-          data: imageFile[0].filepath,
-        };
-
-        // console.log('imageData', imageData);
-
-        const result = await db.collection('images').insertOne(imageData);
-
-        if (result) {
-          console.log('result', result);
-
-          res.status(200).send('Image uploaded and saved to MongoDB.');
-        } else {
-          res.status(500).send('Error saving image to MongoDB.');
-        }
-      } catch (error) {
-        console.error('MongoDB error:', error);
-        res.status(500).send('Error connecting to MongoDB.');
-      } finally {
-        console.log('Finally');
-
-        // client.close();
-      }
-    });
-  } else {
-    res.status(405).send('Method not allowed.');
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
 export default postImage;
-
-// import clientPromise from '@/app/lib/mongodb';
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// export async function postImage(req: NextApiRequest, res: NextApiResponse) {
-//   console.log('post');
-
-//   // console.log('req.body', req.body);
-
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db('wilma-portfolio');
-//     const imageData = req.body;
-
-//     const result = await db.collection('images').insertOne(imageData);
-
-//     res.json(result);
-//   } catch (e) {
-//     console.error(e);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// export default postImage;
