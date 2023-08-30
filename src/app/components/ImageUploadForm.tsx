7c284318d3910906f7bd10b24af4425595b33a95
tovehydmark <tovehydmark@gmail.com>
2023-08-30 22:48:13 +0200

    post image base64 and generate image when picked

--- src/app/components/ImageUploadForm.tsx
+++ src/app/components/ImageUploadForm.tsx
@@ -1,47 +1,45 @@
 import { useState } from 'react';
 
 const ImageUploadForm = () => {
-  const [file, setFile] = useState(null);
+  const [image, setImage] = useState<any>();
 
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.onerror = () => {
+      console.log('reader.error', reader.error);
+    };
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const uploadImage = async () => {
+    try {
+      const response = await fetch('/api/postImage', {
+        method: 'POST',
+        body: JSON.stringify({ base64: image }),
+      });
+      console.log('response', response);
 
-    console.log('file', file);
-
-    if (file) {
-      const formData = new FormData();
-      formData.append('image', file);
-
-      console.log('formData', formData);
-
-      try {
-        const response = await fetch('/api/postImage', {
-          method: 'POST',
-          body: formData,
-        });
-        console.log('response', response);
-
-        if (response.ok) {
-          console.log('Image uploaded successfully!');
-        } else {
-          console.error('Image upload failed.');
-        }
-      } catch (error) {
-        console.error('Error uploading image:', error);
+      if (response.ok) {
+        console.log('Image uploaded successfully!');
+      } else {
+        console.error('Image upload failed.');
       }
+    } catch (error) {
+      console.error('Error uploading image:', error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
-      <button type="submit">Upload</button>
-    </form>
+    <>
+      <input type="file" accept="image/*" onChange={handleSubmit} />
+      <button onClick={uploadImage}>Upload</button>
+      {image === '' || image === null ? '' : <img width={100} height={100} src={image} alt="alttext"></img>}
+    </>
   );
 };
 
