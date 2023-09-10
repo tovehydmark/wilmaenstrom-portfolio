216bc15e3ddcbc3cdd5d5c24576bb339076e0e5e
tovehydmark <tovehydmark@gmail.com>
2023-09-10 22:26:14 +0200

    remove logs and salt as not used

--- src/pages/api/createUser.ts
+++ src/pages/api/createUser.ts
@@ -4,29 +4,19 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import bcryptjs, { genSaltSync } from 'bcryptjs';
 
 export async function createUser(req: NextApiRequest, res: NextApiResponse) {
-  console.log('req.body', req.body);
-
   const client = await clientPromise;
   const db = client.db('wilma-portfolio');
-  const salt = process.env.SALT;
-
-  console.log('salt', salt);
 
   const username = req.body.username;
-
   const plainTextPassword = req.body.password;
 
   // encrypting our password to store in database
-
   const password = await bcryptjs.hash(plainTextPassword, genSaltSync(10).toString());
-  console.log('password', password);
 
   try {
     let newUser = new UserModel();
     newUser.username = username;
     newUser.password = password;
-
-    console.log('newUser', newUser);
 
     const user = await db.collection('users').insertOne(newUser);
 
