7a14d933eff23c654629fd4e47978e62af77d6da
tovehydmark <tovehydmark@gmail.com>
2023-09-11 14:44:57 +0200

    remove unused code

--- src/app/api/models/User.ts
+++ src/app/api/models/User.ts
@@ -4,36 +4,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-
   // admin: Boolean,
 });
 
 export default module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
-
-// import crypto from 'crypto';
-// import mongoose from 'mongoose';
-
-// const UserSchema = new mongoose.Schema({
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-
-//   // admin: Boolean,
-// });
-
-// // // Method to set salt and hash the password for a user
-// // UserSchema.methods.setPassword = function (password: string) {
-// //   // Creating a unique salt for a particular user
-// //   this.salt = crypto.randomBytes(16).toString('hex');
-
-// //   // Hashing user's salt and password with 1000 iterations,
-
-// //   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
-// // };
-
-// // // Method to check the entered password is correct or not
-// // UserSchema.methods.validPassword = function (password: string) {
-// //   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
-// //   return this.hash === hash;
-// // };
-
-// export default module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
