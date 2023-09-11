b5f08d6db621873a2832bbe72dc960f505d85fb4
tovehydmark <tovehydmark@gmail.com>
2023-09-11 14:52:31 +0200

    Display loading when checking data in LS

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -3,10 +3,12 @@ import ImageUploadForm from '@/app/components/ImageUploadForm';
 
 import Illustrations from '@/pages/illustrations';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
   //Todo: Enable to delete image on X
+
+  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
 
   const checkIfUserIsAuthenticated = () => {
     const token = localStorage.getItem('authToken');
@@ -16,6 +18,7 @@ const Dashboard = () => {
     }
 
     try {
+      setUserIsAuthenticated(true);
       return true;
     } catch (error) {
       console.error('Token verification error:', error);
@@ -35,18 +38,24 @@ const Dashboard = () => {
 
   return (
     <>
-      <section className="admin-greeting-section">
-        <h1>Välkommen, Wilma!</h1>
-        <p>Här kan du ladda upp nya bilder och hantera de bilder du laddat upp innan.</p>
-      </section>
-      <section className="admin-upload-image-section">
-        <h2>Ladda upp ny bild</h2>
-        <ImageUploadForm></ImageUploadForm>
-      </section>
-      <section className="admin-gallery">
-        <h2>Galleri</h2>
-        <Illustrations></Illustrations>
-      </section>
+      {userIsAuthenticated ? (
+        <div>
+          <section className="admin-greeting-section">
+            <h1>Välkommen, Wilma!</h1>
+            <p>Här kan du ladda upp nya bilder och hantera de bilder du laddat upp innan.</p>
+          </section>
+          <section className="admin-upload-image-section">
+            <h2>Ladda upp ny bild</h2>
+            <ImageUploadForm></ImageUploadForm>
+          </section>
+          <section className="admin-gallery">
+            <h2>Galleri</h2>
+            <Illustrations></Illustrations>
+          </section>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 };
