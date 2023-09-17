b653ac70b24ca5a63cdb04e73648dbab53ed84bb
tovehydmark <tovehydmark@gmail.com>
2023-09-17 19:28:35 +0200

    post image to azure works

--- src/pages/api/postImage.ts
+++ src/pages/api/postImage.ts
@@ -1,36 +1,15 @@
-import clientPromise from '@/app/lib/mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import azureStorage from '../../app/lib/azureConnection';
 
-export async function postImage(req: NextApiRequest, res: NextApiResponse) {
+export default async function postImage(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const fileBuffer = Buffer.from(req.body, 'base64');
-    console.log('fileBuffer', fileBuffer);
+    const fileBuffer = Buffer.from(req.body.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+    const fileName = 'example-17';
+    const imageUrl = await azureStorage.uploadImageToAzureStorage(fileBuffer, fileName, req.headers['content-type']);
 
-    console.log('req.headers', req.headers);
-
-    const fileName = 'example-10'; // Provide a filename
-
-    const contentType = 'image/png'; // Set the desired content type
-
-    const imageUrl = await azureStorage.uploadImageToAzureStorage(fileBuffer, fileName);
-    console.log('imageUrl', imageUrl);
-
-    res.status(200).json({ imageUrl });
+    res.status(200).json(imageUrl);
   } catch (error) {
-    console.log('error', error);
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Image upload failed' });
   }
-  //   try {
-  //     const client = await clientPromise;
-  //     const db = client.db('wilma-portfolio');
-  //     const result = await db.collection('images').insertOne({ image: req.body });
-
-  //     res.json(result);
-  //   } catch (e) {
-  //     console.error(e);
-  //     res.status(500).json({ error: 'Internal Server Error' });
-  //   }
-  // }
 }
-
-export default postImage;
