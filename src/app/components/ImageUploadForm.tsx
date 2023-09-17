a9655923c81aed6309a38d90a112a96de19248d1
tovehydmark <tovehydmark@gmail.com>
2023-09-17 19:18:41 +0200

    Upload image with set content type works

--- src/app/components/ImageUploadForm.tsx
+++ src/app/components/ImageUploadForm.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 
 const ImageUploadForm = () => {
-  const [image, setImage] = useState<any>();
+  const [image, setImage] = useState<any>(null);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     const reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
-    console.log('e.target.files[0]', e.target.files[0]);
 
     reader.onload = () => {
       setImage(reader.result);
@@ -17,42 +16,40 @@ const ImageUploadForm = () => {
   };
 
   const uploadImage = async () => {
-    if (image) {
-      console.log('image', image);
-      try {
-        const headers = {
-          'Content-Type': 'image/jpeg',
-        };
-        const response = await fetch('/api/postImage', {
-          method: 'POST',
-          body: image,
-          headers: headers,
-        });
-        console.log('response', response);
+    try {
+      const headers = {
+        'Content-Type': 'image/jpeg',
+      };
 
-        if (response.ok) {
-          console.log('Image uploaded successfully!');
-        } else {
-          console.error('Image upload failed.');
-        }
-      } catch (error) {
-        console.error('Error uploading image:', error);
+      const response = await fetch('/api/postImage', {
+        method: 'POST',
+        body: image,
+        headers: headers,
+      });
+
+      if (response.ok) {
+        console.log('Image uploaded successfully!');
+      } else {
+        console.error('Image upload failed.');
       }
-    } else {
-      console.log('No image has been selected');
+    } catch (error) {
+      console.error('Error uploading image:', error);
     }
   };
 
   return (
     <>
-      <section className="upload-file-section">
+      <input type="file" accept="image/*" onChange={handleSubmit} />
+      <button onClick={uploadImage}>Upload Image</button>
+
+      {/* <section className="upload-file-section">
         {' '}
         {!image ? '' : <img className="image-for-upload" src={image} alt="alttext"></img>}
         <div className="upload-file-container">
           <input type="file" accept="image/*" onChange={handleSubmit} />
           <button onClick={uploadImage}>Ladda upp bild</button>
         </div>
-      </section>
+      </section> */}
     </>
   );
 };
