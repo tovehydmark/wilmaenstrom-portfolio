0a5368fc864433f79e0b911fdc3249a123d6b238
tovehydmark <tovehydmark@gmail.com>
2023-09-17 20:11:09 +0200

    increase limit on image size to upload to DB

--- src/pages/api/postImage.ts
+++ src/pages/api/postImage.ts
@@ -1,11 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import azureStorage from '../../app/lib/azureConnection';
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+  maxDuration: 5,
+};
+
 export default async function postImage(req: NextApiRequest, res: NextApiResponse) {
   try {
     const fileBuffer = Buffer.from(req.body.replace(/^data:image\/\w+;base64,/, ''), 'base64');
-    const fileName = 'example-17';
-    const imageUrl = await azureStorage.uploadImageToAzureStorage(fileBuffer, fileName, req.headers['content-type']);
+    const fileName = 'example-1';
+    const imageUrl = await azureStorage.uploadImageToAzureStorage(
+      fileBuffer,
+      fileName,
+      req.headers['content-type'] as string,
+    );
 
     res.status(200).json(imageUrl);
   } catch (error) {
