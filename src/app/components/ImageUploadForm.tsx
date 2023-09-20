548f21aa50d580af6bd825ce3131b02b5652ea40
tovehydmark <tovehydmark@gmail.com>
2023-09-20 15:55:21 +0200

    imageData set from image instead of hard coded

--- src/app/components/ImageUploadForm.tsx
+++ src/app/components/ImageUploadForm.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 
+interface ImageDocument {
+  name: string;
+  type: string;
+}
+
 const ImageUploadForm = () => {
   const [image, setImage] = useState<any>(null);
+  const [imageInfo, setImageInfo] = useState<ImageDocument>();
 
   const handleSubmit = (e) => {
     const reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
+    setImageInfo({ name: e.target.files[0].name, type: e.target.files[0].type });
 
     reader.onload = () => {
       setImage(reader.result);
@@ -17,20 +24,23 @@ const ImageUploadForm = () => {
 
   const uploadImage = async () => {
     try {
-      const headers = {
-        'Content-Type': 'image/jpeg',
-      };
+      if (imageInfo) {
+        const headers = {
+          'Content-Type': imageInfo.type,
+          imageName: imageInfo.name,
+        };
 
-      const response = await fetch('/api/postImage', {
-        method: 'POST',
-        body: image,
-        headers: headers,
-      });
+        const response = await fetch('/api/postImage', {
+          method: 'POST',
+          body: image,
+          headers: headers,
+        });
 
-      if (response.ok) {
-        console.log('Image uploaded successfully!');
-      } else {
-        console.error('Image upload failed.');
+        if (response.ok) {
+          console.log('Image uploaded successfully!');
+        } else {
+          console.error('Image upload failed.');
+        }
       }
     } catch (error) {
       console.error('Error uploading image:', error);
@@ -39,17 +49,14 @@ const ImageUploadForm = () => {
 
   return (
     <>
-      <input type="file" accept="image/*" onChange={handleSubmit} />
-      <button onClick={uploadImage}>Upload Image</button>
-
-      {/* <section className="upload-file-section">
+      <section className="upload-file-section">
         {' '}
         {!image ? '' : <img className="image-for-upload" src={image} alt="alttext"></img>}
         <div className="upload-file-container">
           <input type="file" accept="image/*" onChange={handleSubmit} />
           <button onClick={uploadImage}>Ladda upp bild</button>
         </div>
-      </section> */}
+      </section>
     </>
   );
 };
