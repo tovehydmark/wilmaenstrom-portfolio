cbc204044e6d35142ffb5e514a48f95d421372fd
tovehydmark <tovehydmark@gmail.com>
2023-09-20 17:45:03 +0200

    update model and clean code

--- src/app/api/models/User.ts
+++ src/app/api/models/User.ts
@@ -1,10 +1,8 @@
-import crypto from 'crypto';
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // admin: Boolean,
 });
 
 export default module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
--- src/app/api/models/index.ts
+++ src/app/api/models/index.ts
@@ -1,2 +1,2 @@
 export { default as UserModel } from './User';
-export type { ImageDocument } from './Image';
+export { default as ImageModel } from './Image';
