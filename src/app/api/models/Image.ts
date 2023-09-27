0589223350ac31571a743ecc68194a2210156398
tovehydmark <tovehydmark@gmail.com>
2023-09-27 22:22:21 +0200

    Create interface for images

--- src/app/api/models/Image.ts
+++ src/app/api/models/Image.ts
@@ -1,8 +1,17 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 
 const ImageSchema = new mongoose.Schema({
   imageUrl: { type: String, required: true, unique: true },
   fileName: { type: String, required: true },
+  _id: { type: String, required: false },
 });
 
-export default module.exports = mongoose.models.Image || mongoose.model('Image', ImageSchema);
+export interface ImageDocument extends Document {
+  imageUrl: string;
+  fileName: string;
+  _id?: string;
+}
+
+const ImageModel: Model<ImageDocument> = mongoose.models.Image || mongoose.model<ImageDocument>('Image', ImageSchema);
+
+export default ImageModel;
