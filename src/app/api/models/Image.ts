b0d5483cc52a06197bfaee6fe2d40b0afab904b2
tovehydmark <tovehydmark@gmail.com>
2023-10-04 16:41:48 +0200

    update image model

--- src/app/api/models/Image.ts
+++ src/app/api/models/Image.ts
@@ -5,12 +5,14 @@ const ImageSchema = new mongoose.Schema({
   imageUrl: { type: String, required: true, unique: true },
   fileName: { type: String, required: true },
   _id: { type: ObjectId, required: false },
+  imageDescription: { type: String, required: false },
 });
 
 export interface ImageDocument extends Document {
   imageUrl: string;
   fileName: string;
-  _id?: string;
+  _id?: ObjectId;
+  imageDescription?: string;
 }
 
 const ImageModel: Model<ImageDocument> = mongoose.models.Image || mongoose.model<ImageDocument>('Image', ImageSchema);
