53c58947a8e1c35f16cbe3582b888852262fca30
tovehydmark <tovehydmark@gmail.com>
2023-10-24 15:34:33 +0200

    Component for the individual portfolio image

---
+++ src/app/components/PortfolioImage.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+
+interface Props {
+  id: string | undefined;
+  fileName: string;
+  imageUrl: string;
+  imageDescription: string | undefined;
+}
+
+const PortfolioImage = (Props: Props) => {
+  const { id, fileName, imageUrl, imageDescription } = Props;
+  const [newImageDescription, setNewImageDescription] = useState('');
+  const [updatingImageDescription, setUpdatingImageDescription] = useState(false);
+
+  const editImageDescription = async (id: string | undefined) => {
+    try {
+      const updatedImageDescriptionData = { updatedImageDescription: newImageDescription, id: id };
+      const response = await fetch('/api/updateImageInformation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedImageDescriptionData),
+      });
+
+      if (response.ok) {
+        alert('Bildbeskrivningen är uppdaterad!');
+        setUpdatingImageDescription(false);
+      }
+    } catch {}
+  };
+
+  const deleteImage = async (id: string | undefined, fileName: string) => {
+    try {
+      //Delete image from MongoDB
+      const response = await fetch('/api/deleteImageFromMongoDB', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(id),
+      });
+      if (response.ok) {
+        console.log('Image deleted from MongoDB!');
+
+        //Delete image from Azure
+        try {
+          const response = await fetch('/api/deleteImageFromAzure', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(fileName),
+          });
+
+          if (response.ok) {
+            console.log('Image deleted from Azure!');
+          } else {
+            console.log('Something went wrong, image not deleted from Azure');
+          }
+        } catch (error) {
+          console.log('Error', error);
+        }
+      } else {
+        console.log('Error: Something went wrong, image not deleted from MongoDB.');
+      }
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
+
+  return (
+    <section key={JSON.stringify(id)} className="admin-gallery-image">
+      <button onClick={() => deleteImage(id, fileName)}>Delete</button>
+      <img src={imageUrl} alt={fileName} width={300} height={300} style={{ objectFit: 'cover' }}></img>
+      {updatingImageDescription ? (
+        <div className="image-description-textarea">
+          {' '}
+          <textarea
+            defaultValue={imageDescription}
+            onChange={(e) => setNewImageDescription(e.target.value)}
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button onClick={() => editImageDescription(id)}>Spara</button>
+        </div>
+      ) : (
+        <p>{imageDescription}</p>
+      )}
+
+      <button onClick={() => setUpdatingImageDescription(true)}>Redigera</button>
+    </section>
+  );
+};
+export default PortfolioImage;
