c21b2dbb1226bcba56de98ea44cddba2edcd88bf
tovehydmark <tovehydmark@gmail.com>
2023-10-26 12:53:21 +0200

    side menu for admin

---
+++ src/app/components/admin/SideMenu.tsx
@@ -0,0 +1,10 @@
+const SideMenu = () => {
+  return (
+    <section className="admin-side-menu">
+      <p>Bilder</p>
+      <p>Om</p>
+      <p>Kontakt</p>
+    </section>
+  );
+};
+export default SideMenu;
