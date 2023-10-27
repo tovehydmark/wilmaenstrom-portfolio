36b9474108e8479e984eaf40a3094ad2c735a882
tovehydmark <tovehydmark@gmail.com>
2023-10-27 11:04:11 +0200

    API for posting work experience

---
+++ src/pages/api/userinfo/postWorkexperienceInfo.ts
@@ -0,0 +1,28 @@
+import clientPromise from '@/app/lib/mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { WorkexperienceModel } from '@/app/api/models';
+
+export async function postWorkexperienceInfo(req: NextApiRequest, res: NextApiResponse) {
+  console.log('req.body', req.body);
+
+  const { workplace, city, description } = req.body;
+  const client = await clientPromise;
+  const db = client.db('wilma-portfolio');
+
+  try {
+    let newWorkexperience = new WorkexperienceModel();
+    newWorkexperience.workplace = workplace;
+    newWorkexperience.city = city;
+    newWorkexperience.description = description;
+
+    const workexperience = await db.collection('workexperience').insertOne(newWorkexperience);
+
+    res.status(201).send({ data: workexperience });
+  } catch (error) {
+    console.log('error', error);
+
+    res.status(401).send({ error: error });
+  }
+}
+
+export default postWorkexperienceInfo;
