7273ea8fcbfc77ed30f2a44c04b611055cce1063
tovehydmark <tovehydmark@gmail.com>
2023-10-30 17:24:27 +0100

    basic layout

--- src/pages/admin/dashboard/userInfo.tsx
+++ src/pages/admin/dashboard/userInfo.tsx
@@ -65,50 +65,52 @@ const UserInfo = () => {
     <>
       {userIsAuthenticated ? (
         <>
-          <SideMenu></SideMenu>
-          <h1>Om mig</h1>
-          <p>Här kan du lägga till och uppdatera information om dig.</p>
-          <h2>Utbildning</h2>
-          <button onClick={() => setAddEducation(true)}>Lägg till utbildning</button>
-          {addEducation ? <EducationCard onSave={() => setAddEducation(false)}></EducationCard> : <></>}
+          <section className="about-container">
+            <SideMenu></SideMenu>
+            <h1>Om mig</h1>
+            <p>Här kan du lägga till och uppdatera information om dig.</p>
+            <h2>Utbildning</h2>
+            <button onClick={() => setAddEducation(true)}>Lägg till utbildning</button>
+            {addEducation ? <EducationCard onSave={() => setAddEducation(false)}></EducationCard> : <></>}
 
-          <section className="education-section">
-            {education ? (
-              education.map((edu) => {
-                return (
-                  <article key={edu._id}>
-                    <h3>{edu.degree}</h3>
-                    <h4>{edu.school}</h4>
-                    <p>{edu.description}</p>
-                  </article>
-                );
-              })
+            <section className="education-section">
+              {education ? (
+                education.map((edu) => {
+                  return (
+                    <article key={edu._id}>
+                      <h3>{edu.degree}</h3>
+                      <h4>{edu.school}</h4>
+                      <p>{edu.description}</p>
+                    </article>
+                  );
+                })
+              ) : (
+                <></>
+              )}
+            </section>
+
+            <h2>Arbetserfarenhet</h2>
+            <button onClick={() => setAddWorkexperience(true)}>Lägg till arbetserfarenhet</button>
+            {addWorkexperience ? (
+              <WorkexperienceCard onSave={() => setAddWorkexperience(false)}></WorkexperienceCard>
             ) : (
               <></>
             )}
-          </section>
-
-          <h2>Arbetserfarenhet</h2>
-          <button onClick={() => setAddWorkexperience(true)}>Lägg till arbetserfarenhet</button>
-          {addWorkexperience ? (
-            <WorkexperienceCard onSave={() => setAddWorkexperience(false)}></WorkexperienceCard>
-          ) : (
-            <></>
-          )}
-          <section className="work-experience-section">
-            {workexperience ? (
-              workexperience.map((experience) => {
-                return (
-                  <article key={experience._id}>
-                    <h3>{experience.workplace}</h3>
-                    <h4>{experience.city}</h4>
-                    <p>{experience.description}</p>
-                  </article>
-                );
-              })
-            ) : (
-              <></>
-            )}
+            <section className="work-experience-section">
+              {workexperience ? (
+                workexperience.map((experience) => {
+                  return (
+                    <article key={experience._id}>
+                      <h3>{experience.workplace}</h3>
+                      <h4>{experience.city}</h4>
+                      <p>{experience.description}</p>
+                    </article>
+                  );
+                })
+              ) : (
+                <></>
+              )}
+            </section>
           </section>
         </>
       ) : (
