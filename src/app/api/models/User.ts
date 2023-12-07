25ba1cfb38089ea129c60c879a246260e0bb4bbc
tovehydmark <tovehydmark@gmail.com>
2023-12-07 17:05:54 +0100

    put headerImageUrl in User

--- src/app/api/models/User.ts
+++ src/app/api/models/User.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  headerImageUrl: { type: String, required: false, unique: true },
 });
 
 export default module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
