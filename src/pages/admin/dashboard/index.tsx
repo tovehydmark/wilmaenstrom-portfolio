be34b5d1e7a11d2b57100285d029c4472333ba3a
tovehydmark <tovehydmark@gmail.com>
2023-12-20 18:37:51 +0100

    check for header

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -69,6 +69,7 @@ const Dashboard = () => {
               <ImageUploadForm
                 apiString={'postImageToAzure'}
                 onSave={() => setUploadedNewImage(!uploadedNewImage)}
+                isHeader={false}
               ></ImageUploadForm>
             </section>
             <hr />
