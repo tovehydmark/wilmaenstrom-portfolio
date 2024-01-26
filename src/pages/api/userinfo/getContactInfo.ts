edf504e4a7c6753cb63e029d8ab9ef782545e36b
tovehydmark <tovehydmark@gmail.com>
2024-01-26 19:06:03 +0100

    api get contact info

---
+++ src/pages/api/userinfo/getContactInfo.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '@/app/lib/mongodb';
+import { AboutDocument } from '@/app/api/models/about';
+import { ContactDocument } from '@/app/api/models/contact';
+
+export async function getContactInfo(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('wilma-portfolio');
+
+    const contactCollection = db.collection<ContactDocument>('contact');
+
+    //TODO: Check this qurey
+    const contact = await contactCollection.findOne();
+
+    if (!contact) {
+      return res.status(404).json({ status: 'error', error: 'No contact info found' });
+    }
+
+    return res.status(200).json({ contact: contact });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ status: 'error', error: 'Internal server error' });
+  }
+}
+
+export default getContactInfo;
