06298d7399cbef48aeb31161d7a4b35bf3201fd6
tovehydmark <tovehydmark@gmail.com>
2024-01-26 19:40:18 +0100

    View contact card and route to get contact info

--- src/pages/admin/dashboard/contactInfo.tsx
+++ src/pages/admin/dashboard/contactInfo.tsx
@@ -1,10 +1,16 @@
 import SideMenu from '@/app/components/admin/SideMenu';
+import ContactCard from '@/app/components/admin/about/ContactCard';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 const ContactInfo = () => {
   const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
   const router = useRouter();
+
+  const [addContactData, setAddContactData] = useState(false);
+  const [description, setDescription] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
 
   const checkIfUserIsAuthenticated = () => {
     const token = localStorage.getItem('authToken');
@@ -28,12 +34,44 @@ const ContactInfo = () => {
     }
   }, [router]);
 
+  useEffect(() => {
+    (async () => {
+      //Get about data
+      try {
+        const response = await fetch('/api/userinfo/getContactInfo');
+        const data: any = await response.json();
+
+        if (data.contact.description) {
+          setDescription(data.contact.description);
+        }
+        if (data.contact.phone) {
+          setPhone(data.contact.phone);
+        }
+        if (data.contact.email) {
+          setEmail(data.contact.email);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [addContactData]);
+
   return (
     <>
       {userIsAuthenticated ? (
         <>
           <SideMenu></SideMenu>
-          <p>CONTACT info </p>
+          <h2>Kontakt</h2>
+          <p>Det är bra med kontaktuppgifter om någon vill kontakta dig</p>
+          <button
+            onClick={() => setAddContactData(!addContactData)}
+            className={!addContactData ? 'primary-btn center' : 'secondary-btn center'}
+          >
+            {!addContactData ? 'Redigera' : 'Avbryt'}
+          </button>
+
+          {addContactData ? <ContactCard onSave={() => addContactData(false)}></ContactCard> : <></>}
+          <hr />
         </>
       ) : (
         <div>Loading...</div>
