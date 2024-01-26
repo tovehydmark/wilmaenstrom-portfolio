1f059cb566623de53d2e046f544106159fc1cd37
tovehydmark <tovehydmark@gmail.com>
2024-01-26 19:05:50 +0100

    contact model

--- src/app/api/models/index.ts
+++ src/app/api/models/index.ts
@@ -3,3 +3,4 @@ export { default as ImageModel } from './Image';
 export { default as EducationModel } from './education';
 export { default as WorkexperienceModel } from './workexperience';
 export { default as AboutModel } from './about';
+export { default as ContactModel } from './contact';
